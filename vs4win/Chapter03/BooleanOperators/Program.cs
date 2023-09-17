11c32ccff92fd71cfda5fb827b7d4683360b5bd2
Daniel Ojeda Ubeda <43439678+Djeda8@users.noreply.github.com>
2023-09-17 09:31:26 +0100

    Changes chapter03

--- vs4win/Chapter03/BooleanOperators/Program.cs
+++ vs4win/Chapter03/BooleanOperators/Program.cs
@@ -13,11 +13,11 @@ WriteLine($"a    | {a ^ a,-5} | {a ^ b,-5} ");
 WriteLine($"b    | {b ^ a,-5} | {b ^ b,-5} ");
 
 WriteLine();
-WriteLine($"a & DoStuff() = {a & DoStuff()}"); 
+WriteLine($"a & DoStuff() = {a & DoStuff()}");
 WriteLine($"b & DoStuff() = {b & DoStuff()}");
 
 WriteLine();
-WriteLine($"a & DoStuff() = {a && DoStuff()}"); 
+WriteLine($"a & DoStuff() = {a && DoStuff()}");
 WriteLine($"b & DoStuff() = {b && DoStuff()}");
 
 static bool DoStuff()
