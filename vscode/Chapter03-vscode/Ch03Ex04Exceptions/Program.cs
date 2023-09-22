0f23843ad3cb7ba8cc5000a79bcc523d737ee557
Daniel Ojeda Ubeda <43439678+Djeda8@users.noreply.github.com>
2023-09-22 18:00:41 +0100

    Change execise 4

--- vscode/Chapter03-vscode/Ch03Ex04Exceptions/Program.cs
+++ vscode/Chapter03-vscode/Ch03Ex04Exceptions/Program.cs
@@ -1,4 +1,6 @@
-﻿int aa,bb;
+﻿using static System.Convert;
+
+int aa,bb;
 Write("Enter a number between 0 and 255: ");
 string a = ReadLine();
 Write("Enter a number between 0 and 255: ");
@@ -6,7 +8,7 @@ string b = ReadLine();
 
 try
 {
-    aa = Convert.ToInt32(a);
+    aa = ToInt32(a);
     //aa = int.Parse(a);
     bb = Convert.ToInt32(b);
     //bb = int.Parse(b);
