f59a81efbe341b44ab4247bdf177a4a500c53a66
Daniel Ojeda Ubeda <43439678+Djeda8@users.noreply.github.com>
2024-01-22 19:14:37 +0000

    Chapter07

--- vscode/Chapter07-vscode/AssembliesAndNamespaces/Program.cs
+++ vscode/Chapter07-vscode/AssembliesAndNamespaces/Program.cs
@@ -1,8 +1,22 @@
 ﻿using System.Xml.Linq; // XDocument
 using System; // String
+using Packt.Shared;
 
 XDocument doc = new();
 
 string s1 = "Hello";
 String s2 = "World";
 WriteLine($"{s1} {s2}");
+
+Write("Enter a color value in hex: "); 
+string? hex = ReadLine(); // or "00ffc8"
+WriteLine("Is {0} a valid color value? {1}",
+  arg0: hex, arg1: hex.IsValidHex());
+Write("Enter a XML element: "); 
+string? xmlTag = ReadLine(); // or "<h1 class=\"<\" />"
+WriteLine("Is {0} a valid XML element? {1}", 
+  arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+Write("Enter a password: "); 
+string? password = ReadLine(); // or "secretsauce"
+WriteLine("Is {0} a valid password? {1}",
+  arg0: password, arg1: password.IsValidPassword());
